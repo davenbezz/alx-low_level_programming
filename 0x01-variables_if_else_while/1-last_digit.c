d0b87cbfbd2f9609512f866f3e15af16a2d1085d
Dawit <it.dawit.bezabih@gmail.com>
2022-04-08 13:28:08 -0700

    task 1 edited

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,9 +6,13 @@
 
 /**
  * main - Entry point
- * Description: Prints a number and it's last digit along with whether its last digit is 0, greater than 5, or less than 6 and not 0.
+ * Description: Prints a number and it's last digit along with
+ *  whether its last digit is 0, greater than 5, or less than
+ *   6 and not 0.
+ *
  * Return: 0
  */
+
 
 int main(void)
 
