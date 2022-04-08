88d0350a685ee7a3be9e3c423cf008201fac485c
Dawit <it.dawit.bezabih@gmail.com>
2022-04-08 13:44:50 -0700

    finished tasks from 2 - 9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: Print lowercase alphabet backwards
+ * Return: 0
+ */
+
+
+int main(void)
+
+{
+
+	char c;
+
+
+
+	for (c = 'z'; c >= 'a'; --c)
+
+		putchar(c);
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
