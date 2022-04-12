227021a8c56b9bd842c6ce7556907bd8c7af61c5
Dawit <it.dawit.bezabih@gmail.com>
2022-04-12 13:48:28 -0700

    task 5 edited

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n - the number in which the sign will be printed
+ * @n: the number in which the sign will be printed
  * Return: 1 if the number is greater than zero,
  * 0 if the number is zero,
  * -1 if the number is less than zero.
